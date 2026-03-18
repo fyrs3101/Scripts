d916cdf36082f6cae14f2aa438de09ebb096bf26
fyrs3101 <61998213+fyrs3101@users.noreply.github.com>
2026-03-18 09:27:28 +0800

    Create yangshipin.js

---
+++ yangshipin.js
@@ -0,0 +1,14 @@
+[MITM]
+hostname = *.yangshipin.cn, *.cctv.cn
+[rewrite_local]
+# 会员解锁
+^https?:\/\/mobile\.yangshipin\.cn\/vapi\/user\/vip url script-response-body https://raw.githubusercontent.com/Moli-X/Resources/main/Script/yangshipin.js
+# 广告去除
+^https?:\/\/ads\.yangshipin\.cn\/.+ url reject
+let obj = JSON.parse($response.body);
+obj.data.vip = {
+  "expireTime": "2099-12-31",
+  "type": 1,
+  "status": 1
+};
+$done({body: JSON.stringify(obj)});
